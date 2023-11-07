5152fd08981e13d68113c51849e555ab4037fa54
Kamil R <kamilroslon@MacBook-Pro-Kamil-4.local>
2023-11-07 21:38:46 +0100

    Mod 2 magit methods __repr__ Apple and Potato

--- mod_2_Classes_ex_2/shop/Potato.py
+++ mod_2_Classes_ex_2/shop/Potato.py
@@ -5,5 +5,8 @@ class Potato:
         self.price_per_kg = price_per_kg
         self.price = 0
 
+    def __repr__(self):
+        return f"Name: {self.species_name}, Size: {self.size}, Price per kg: {self.price_per_kg}"
+
     def calculate_price(self, weight):
         self.price = self.price_per_kg * weight
