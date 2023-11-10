1eb5b29a834f23ed4039130db197dc4a537d0fd3
Kamil R <kamilroslon@MacBook-Pro-Kamil-4.local>
2023-11-10 20:56:01 +0100

    Mod 2 magit encapsulation

--- mod_2_Classes_ex_2/shop/Order.py
+++ mod_2_Classes_ex_2/shop/Order.py
@@ -1,18 +1,19 @@
+from shop.Product import Product
 class Order:
     def __init__(self, first_name, last_name, elements_list=None):
         self.first_name = first_name
         self.last_name = last_name
         if elements_list is None:
             elements_list = []
-        self.elements_list = elements_list
+        self._elements_list = elements_list
         # total_price = 0
         # for product in products_list:
         #     total_price += product.unit_price * product.pieces
-        self.summary_price = self.calculate_prices()
+        self.summary_price = self._calculate_prices()
 
     def __str__(self):
         elements = ""
-        for element in self.elements_list:
+        for element in self._elements_list:
             elements += "\n"
             elements += f"Product: {element.name} | Pieces: {element.pieces} | Price per piece: {element.unit_price} | Total row: {element.unit_price * element.pieces}"
         elements += "\n" * 2
@@ -20,26 +21,31 @@ class Order:
         return elements
 
     def __len__(self):
-        return len(self.elements_list)
+        return len(self._elements_list)
 
     def __eq__(self, other):
         if self.__class__ != other.__class__:
             return NotImplemented
-        if len(self.elements_list) != len(other.elements_list):
+        if len(self._elements_list) != len(other._elements_list):
             return False
         if self.first_name != other.first_name or self.last_name != other.last_name:
             return False
-        for element in self.elements_list:
-            if element not in other.elements_list:
+        for element in self._elements_list:
+            if element not in other._elements_list:
                 return False
         return True
 
-    def calculate_prices(self):
+    def _calculate_prices(self):
         total_price = 0
-        for element in self.elements_list:
+        for element in self._elements_list:
             total_per_row = element.unit_price * element.pieces
             total_price += total_per_row
         return total_price
+
+    def add_product_to_order(self, name, category, unit_price, pieces):
+        new_product = Product(name, category, unit_price, pieces)
+        self._elements_list.append(new_product)
+
 
 
 class OrderElement:
