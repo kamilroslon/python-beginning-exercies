62610722b523cc7984af3f7bcd6e6215f1a81dc7
Kamil R <kamilroslon@MacBook-Pro-Kamil-4.local>
2023-11-26 17:47:19 +0100

    Mod 2 Refactoring exercise 1 changes more

--- mod_2_Classes_ex_2/shop/data_generator.py
+++ mod_2_Classes_ex_2/shop/data_generator.py
@@ -1,14 +1,15 @@
 from shop.Product import Product
+from shop.Order import Order
 import random
 
+MIN_UNIT_PIECE = 1
+MAX_UNIT_PIECE = 15
+MIN_UNIT_PRICE = 1
 MAX_UNIT_PRICE = 30
-MAX_NUMBER_OF_PRODUCTS = 20
 
 def order_generator(number_of_products=None):
     elements = []
     if number_of_products is not None:
-        # number_of_elements = random.randint(1, MAX_NUMBER_OF_PRODUCTS)
-
         for element in range(number_of_products):
             random_number = random.randint(1, 79)
             products_name = f"Product_name_{element}"
@@ -17,13 +18,13 @@ def order_generator(number_of_products=None):
             unit_piece = random.randint(1, 15)
             elements.append(Product(products_name, products_category, unit_price, unit_piece))
     else:
-        number_of_elements = random.randint(1, MAX_NUMBER_OF_PRODUCTS)
+        number_of_elements = random.randint(1, Order.MAX_ELEMENTS)
         elements = []
         for element in range(number_of_elements):
             random_number = random.randint(1, 79)
             products_name = f"Product_name_{element}"
             products_category = random_number
-            unit_price = random.randint(1, MAX_UNIT_PRICE)
-            unit_piece = random.randint(1, 15)
+            unit_price = random.randint(MIN_UNIT_PRICE, MAX_UNIT_PRICE)
+            unit_piece = random.randint(MIN_UNIT_PIECE, MAX_UNIT_PIECE)
             elements.append(Product(products_name, products_category, unit_price, unit_piece))
     return elements
